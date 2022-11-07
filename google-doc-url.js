4a3bc9d0fe6b2ae7ed7e590d16b09a2bd4ef9334
Tropic35 <117666994+Tropic35@users.noreply.github.com>
2022-11-07 17:14:42 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -3,3 +3,4 @@ var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejk
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+https://docs.google.com/spreadsheets/d/10PaNlb2sV8kA-UARidJ6rXFTU4hsoTeEee1owy3yjfU/edit#gid=0
